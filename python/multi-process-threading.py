806f5b79b1b3e3dd306b955a4478caf5e55e8c88
susmi7991 <82033383+susmi7991@users.noreply.github.com>
2025-09-08 10:18:02 +0530

    Create multi-process-threading.py

---
+++ python/multi-process-threading.py
@@ -0,0 +1,166 @@
+## PRocesses that run in parallel
+### CPU-Bound Tasks-Tasks that are heavy on CPU usage (e.g., mathematical computations, data processing).
+## PArallel execution- Multiple cores of the CPU
+
+import multiprocessing
+
+import time
+
+def square_numbers():
+    for i in range(5):
+        time.sleep(1)
+        print(f"Square: {i*i}")
+
+def cube_numbers():
+    for i in range(5):
+        time.sleep(1.5)
+        print(f"Cube: {i * i * i}")
+
+if __name__=="__main__":
+
+    ## create 2 processes
+    p1=multiprocessing.Process(target=square_numbers)
+    p2=multiprocessing.Process(target=cube_numbers)
+    t=time.time()
+
+    ## start the process
+    p1.start()
+    p2.start()
+
+    ## Wait for the process to complete
+    p1.join()
+    p2.join()
+
+    finished_time=time.time()-t
+    print(finished_time)
+
+
+### Multithreading
+## When to use Multi Threading
+###I/O-bound tasks: Tasks that spend more time waiting for I/O operations (e.g., file operations, network requests).
+###  Concurrent execution: When you want to improve the throughput of your application by performing multiple operations concurrently.
+
+import threading
+import time
+
+def print_numbers():
+    for i in range(5):
+        time.sleep(2)
+        print(f"Number:{i}")
+
+def print_letter():
+    for letter in "abcde":
+        time.sleep(2)
+        print(f"Letter: {letter}")
+
+##create 2 threads
+t1=threading.Thread(target=print_numbers)
+t2=threading.Thread(target=print_letter)
+
+t=time.time()
+## start the thread
+t1.start()
+t2.start()
+
+### Wait for the threads to complete
+t1.join()
+t2.join()
+
+finished_time=time.time()-t
+print(finished_time)
+
+
+'''
+Real-World Example: Multiprocessing for CPU-bound Tasks
+Scenario: Factorial Calculation
+Factorial calculations, especially for large numbers, 
+involve significant computational work. Multiprocessing 
+can be used to distribute the workload across multiple 
+CPU cores, improving performance.
+
+'''
+
+import multiprocessing
+import math
+import sys
+import time
+
+# Increase the maximum number of digits for integer conversion
+sys.set_int_max_str_digits(100000)
+
+## function to compute factorials of a given number 
+
+def computer_factorial(number):
+    print(f"Computing factorial of {number}")
+    result=math.factorial(number)
+    print(f"Factorial of {number} is {result}")
+    return result
+
+if __name__=="__main__":
+    numbers=[5000,6000,700,8000]
+
+    start_time=time.time()
+
+    ##create a pool of worker processes
+    with multiprocessing.Pool() as pool:
+        results=pool.map(computer_factorial,numbers)
+
+    end_time=time.time()
+
+    print(f"Results: {results}")
+    print(f"Time taken: {end_time - start_time} seconds")
+
+
+
+
+
+## Webscrapping
+
+'''
+Real-World Example: Multithreading for I/O-bound Tasks
+Scenario: Web Scraping
+Web scraping often involves making numerous network requests to 
+fetch web pages. These tasks are I/O-bound because they spend a lot of
+time waiting for responses from servers. Multithreading can significantly
+improve the performance by allowing multiple web pages to be fetched concurrently.
+
+'''
+
+'''
+
+https://python.langchain.com/v0.2/docs/introduction/
+
+https://python.langchain.com/v0.2/docs/concepts/
+
+https://python.langchain.com/v0.2/docs/tutorials/
+'''
+
+import threading
+import requests
+from bs4 import BeautifulSoup
+
+urls=[
+'https://python.langchain.com/v0.2/docs/introduction/',
+
+'https://python.langchain.com/v0.2/docs/concepts/',
+
+'https://python.langchain.com/v0.2/docs/tutorials/'
+
+]
+
+def fetch_content(url):
+    response=requests.get(url)
+    soup=BeautifulSoup(response.content,'html.parser')
+    print(f'Fetched {len(soup.text)} characters from {url}')
+
+threads=[]
+
+for url in urls:
+    thread=threading.Thread(target=fetch_content,args=(url,))
+    threads.append(thread)
+    thread.start()
+
+for thread in threads:
+    thread.join()
+
+print("All web pages fetched")
